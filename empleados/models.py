083b7a6201542bede96f4504fc76e56b018107d0
ignaciooocs <naxomc25@gmail.com>
2024-12-05 22:05:56 -0300

    REST FRAMEWORK was implemented

--- empleados/models.py
+++ empleados/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Empleado(models.Model):
+    nombre = models.CharField(max_length=100, verbose_name="Nombre")
+    apellido = models.CharField(max_length=100, verbose_name="Apellido")
+    email = models.EmailField(unique=True, verbose_name="Correo Electrónico")
+    telefono = models.CharField(max_length=15, verbose_name="Teléfono", blank=True, null=True)
+    puesto = models.CharField(max_length=100, verbose_name="Puesto de Trabajo")
+    salario = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Salario")
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido} - {self.puesto}"
