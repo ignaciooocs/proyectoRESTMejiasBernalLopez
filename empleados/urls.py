f5be02185578e776a524442c83bcfac87830c95b
ignaciooocs <naxomc25@gmail.com>
2024-12-05 22:06:28 -0300

    REST FRAMEWORK was implemented

--- empleados/urls.py
+++ empleados/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index),
-    path('empleadosListApi', views.list),
-    path('empleadosListApi/<int:pk>', views.empleado, name='empleado'),
+    path('empleadosListApi', views.lista_empleados),
+    path('empleadosListApi/<int:pk>', views.detalle_empleado, name='empleado'),
+    path('empleadosListApi/crear/', views.crear_empleado, name='crear_empleado'),
 ]
--- empleados/views.py
+++ empleados/views.py
@@ -1,12 +1,46 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Empleado
+from .serializers import EmpleadoSerializer
 from django.shortcuts import render
 
-# Create your views here.
+@api_view(['GET'])
+def lista_empleados(request):
+    empleados = Empleado.objects.all()
+    serializer = EmpleadoSerializer(empleados, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def detalle_empleado(request, pk):
+    try:
+        empleado = Empleado.objects.get(pk=pk)
+    except Empleado.DoesNotExist:
+        return Response({'error': 'Empleado no encontrado'}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = EmpleadoSerializer(empleado)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = EmpleadoSerializer(empleado, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        empleado.delete()
+        return Response({'mensaje': 'Empleado eliminado'}, status=status.HTTP_204_NO_CONTENT)
+    
+@api_view(['POST'])
+def crear_empleado(request):
+    if request.method == 'POST':
+        serializer = EmpleadoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 def index(request):
     return render(request, 'index.html')
-
-def list(request):
-    return render(request, 'empleadosList.html')
-
-def empleado(request, pk):  # Acepta el parámetro pk
-    context = {'pk': pk}  # Puedes pasar el pk al template si es necesario
-    return render(request, 'empleado.html', context)
